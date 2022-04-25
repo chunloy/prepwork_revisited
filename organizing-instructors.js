e97fbf1b0c128ac1a23c93ab4b5d4c91c1b07985
Aaron Au <aaron.au@me.com>
2022-04-24 18:24:57 -0600

    Initial commit. Implement function to organize list by course.

---
+++ organizing-instructors.js
@@ -0,0 +1,53 @@
+const list1 = [
+  { name: "Samuel", course: "iOS" },
+  { name: "Victoria", course: "Web" },
+  { name: "Karim", course: "Web" },
+  { name: "Donald", course: "Web" }
+];
+
+const list2 = [
+  { name: "Brendan", course: "Blockchain" },
+  { name: "David", course: "Web" },
+  { name: "Martha", course: "iOS" },
+  { name: "Carlos", course: "Web" }
+];
+
+//expected output is a key array pair
+// {
+//   iOS: ["Samuel"],
+//     Web: ["Victoria", "Karim", "Donald"];
+// }
+// {
+//   Blockchain: ["Brendan"],
+//     Web: ["David", "Carlos"],
+//       iOS: ["Martha"];
+// }
+
+const getCourseList = function(instructors) {
+  const courseList = [];
+  for (const instructor of instructors) {
+    if (!courseList.includes(instructor.course)) {
+      courseList.push(instructor.course);
+    }
+  }
+  return courseList;
+};
+
+const organizeInstructors = function(instructors, callback) {
+  const courseList = callback(instructors);
+  const output = {};
+
+  for (const course of courseList) {
+    output[course] = [];
+    for (const instructor of instructors) {
+      if (instructor.course === course) {
+        output[course].push(instructor.name);
+      }
+    }
+  }
+
+  return output;
+};
+
+console.log(organizeInstructors(list1, getCourseList));
+console.log(organizeInstructors(list2, getCourseList));
